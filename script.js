2af1cea269088e4e4eea90983854eb239cab20f8
acandoo <117209328+acandoo@users.noreply.github.com>
2025-06-10 15:06:22 -0400

    refactor: restructure grid creation logic and enhance styling

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 const container = document.getElementById("container");
 
 // Create a grid of 16x16 squares
-for (const i of Array(16).keys()) {
-    const verticalDiv = document.createElement("div");
-    verticalDiv.style.display = "flex";
-    verticalDiv.style.flexDirection = "row";
-    for (const j of Array(16).keys()) {
-        const horizontalDiv = document.createElement("div");
-        horizontalDiv.className = "square";
-        horizontalDiv.style.width = "20px";
-        horizontalDiv.style.height = "20px";
-        horizontalDiv.style.border = "1px solid black";
-        verticalDiv.appendChild(horizontalDiv);
+function createGrid(rows, cols, container) {
+    for (const i of Array(rows).keys()) {
+        const verticalDiv = document.createElement("div");
+        verticalDiv.style.display = "flex";
+        verticalDiv.style.flexDirection = "row";
+        verticalDiv.style.flexGrow = "1";
+        for (const j of Array(cols).keys()) {
+            const square = document.createElement("div");
+            square.className = "square";
+            square.style.border = "1px solid black";
+            square.style.flexGrow = "1";
+            square.innerText = j;
+            verticalDiv.appendChild(square);
+        }
+        container.appendChild(verticalDiv);
     }
-    document.body.appendChild(verticalDiv);
 }
+
+createGrid(16, 16, container);
+document.querySelectorAll(".square").forEach((square) => {
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "black";
+    });
+});
